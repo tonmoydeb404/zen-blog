c956fffc1cbcdc1a1f898b13e78c96a59deebe34
tonmoydeb404 <tonmoydeb404@gmail.com>
2022-06-23 03:14:52 +0600

    added progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,33 @@
+import dynamic from "next/dynamic";
 import Layout from "../components/Layout";
 import { BookmarksContextProvider } from "../contexts/BookmarksContext";
 import { ThemeContextProvider } from "../contexts/ThemeContext";
+import { siteInfo } from "../lib/constant";
 import "../scss/globals.scss";
+
+const NextNProgress = dynamic(() => import("nextjs-progressbar"), {
+  ssr: false,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeContextProvider>
-      <BookmarksContextProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </BookmarksContextProvider>
-    </ThemeContextProvider>
+    <>
+      <NextNProgress
+        color={siteInfo.color}
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        options={{ showSpinner: false }}
+        style={{ zIndex: 999999 }}
+      />
+      <ThemeContextProvider>
+        <BookmarksContextProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </BookmarksContextProvider>
+      </ThemeContextProvider>
+    </>
   );
 }
 
